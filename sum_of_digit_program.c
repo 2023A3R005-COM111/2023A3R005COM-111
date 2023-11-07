e95daaf526c90b336442dc22ad06ecb3592aaaa1
Jagjot Singh <145693437+2023A3R005-COM111@users.noreply.github.com>
2023-11-07 10:05:29 +0530

    Add files via upload

--- sum_of_digit_program.c
+++ sum_of_digit_program.c
@@ -1,15 +1,24 @@
 #include<stdio.h>
 int main()
 {
-    int a,sum=0,remainder;
-    printf("enter the first number  of three number: ");
-    scanf("%d",&a);
-    while(a>0)
+    int i,sum=0,remainder;
+    printf("enter the number: ");
+    scanf("%d",&i);
+    if (i<0)
     {
-        remainder=a%10;
-        sum=sum+remainder;
-        a=a/10;
+        printf("enter a positive number");
     }
-    printf("sum of the number is: %d\n",sum);
+    else
+    {
+    while (i>0)
+    {
+        /* code */
+        remainder=i%10;
+        sum=sum+remainder;
+        i=i/10;
+    }
+    printf("Sum of digit: %d",sum);
+    }
     return 0;
+    
 }
